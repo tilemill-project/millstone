17f94cc2ad5caef288c540c0e65b5d042a71246a
Dane Springmeyer <dane@dbsgeo.com>
2012-09-20 16:29:37 -0700

    fix and add tests - closes #68, #69, #70, #72, and #73

--- test/localize.test.js
+++ test/localize.test.js
@@ -12,23 +12,22 @@ var rm = require('./support.js').rm;
 var existsSync = require('fs').existsSync || require('path').existsSync;
 
 beforeEach(function(){
-  rm(path.join(__dirname, 'tmp'));
+  rm(path.join(__dirname, '/tmp/millstone-test'));
 })
 
 it('correctly localizes remote image/svg files', function(done) {
     var mml = JSON.parse(fs.readFileSync(path.join(__dirname, 'markers/project.mml')));
-    rm(path.join(__dirname, 'tmp'));
 
     var options = {
         mml: mml,
         base: path.join(__dirname, 'markers'),
-        cache: '/tmp'
+        cache: '/tmp/millstone-test'
     };
 
     millstone.resolve(options, function(err, resolved) {
         assert.equal(err,undefined,err);
         assert.equal(resolved.Stylesheet[0].id, 'style.mss');
-        assert.equal(resolved.Stylesheet[0].data, '#points { one/marker-file: url(/tmp/e33af80e-Cup_of_coffee.svg); two/marker-file: url(\'/tmp/e33af80e-Cup_of_coffee.svg\'); three/marker-file: url(/tmp/ce5fcc0b-SVG-logo.svg);}');
+        assert.equal(resolved.Stylesheet[0].data, '// a url like https:example.com in the comments\n#points { one/marker-file: url(/tmp/millstone-test/e33af80e-Cup_of_coffee.svg); two/marker-file: url(\'/tmp/millstone-test/e33af80e-Cup_of_coffee.svg\'); three/marker-file: url(/tmp/millstone-test/ce5fcc0b-SVG-logo.svg); four/marker-file: url("/tmp/millstone-test/c953e0d1-pin-m-fast-food+AA0000.png"); five/marker-file:url(/tmp/millstone-test/a2278544-octocat-svg/a2278544-octocat-svg.svg); }');
         assert.deepEqual(resolved.Layer, [
             {
                 "name": "points",
--- test/markers/style.mss
+++ test/markers/style.mss
@@ -1,1 +1,2 @@
-#points { one/marker-file: url(http://upload.wikimedia.org/wikipedia/commons/7/72/Cup_of_coffee.svg); two/marker-file: url('http://upload.wikimedia.org/wikipedia/commons/7/72/Cup_of_coffee.svg'); three/marker-file: url(http://upload.wikimedia.org/wikipedia/en/c/ce/SVG-logo.svg);}
+// a url like https:example.com in the comments
+#points { one/marker-file: url(http://upload.wikimedia.org/wikipedia/commons/7/72/Cup_of_coffee.svg); two/marker-file: url('http://upload.wikimedia.org/wikipedia/commons/7/72/Cup_of_coffee.svg'); three/marker-file: url(http://upload.wikimedia.org/wikipedia/en/c/ce/SVG-logo.svg); four/marker-file: url("http://a.tiles.mapbox.com/v3/marker/pin-m-fast-food+AA0000.png"); five/marker-file:url(http://ds.io/octocat-svg); }
