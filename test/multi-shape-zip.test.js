dfc7f7406269be2208357c041eb5aac210e6b376
Dane Springmeyer <dane@dbsgeo.com>
2013-08-08 23:21:29 -0400

    add test for #99

---
+++ test/multi-shape-zip.test.js
@@ -0,0 +1,37 @@
+var fs = require('fs');
+var path = require('path');
+var assert = require('assert');
+
+// switch to 'development' for more verbose logging
+//process.env.NODE_ENV = 'production'
+var utils = require('../lib/util.js');
+var millstone = require('../lib/millstone');
+var tests = module.exports = {};
+var rm = require('./support.js').rm;
+
+var existsSync = require('fs').existsSync || require('path').existsSync;
+
+beforeEach(function(){
+  rm(path.join(__dirname, '/tmp/millstone-test'));
+})
+
+// https://github.com/mapbox/millstone/issues/99
+it('correctly throws if zipfile contains multiple shapefiles', function(done) {
+    var mml = JSON.parse(fs.readFileSync(path.join(__dirname, 'multi-shape-zip/project.mml')));
+    
+    var cache = '/tmp/millstone-test';
+    var options = {
+        mml: mml,
+        base: path.join(__dirname, 'multi-shape-zip'),
+        cache: cache
+    };
+
+    try {
+        fs.mkdirSync(options.cache, 0777);
+    } catch (e) {}
+    
+    millstone.resolve(options, function(err, resolved) {
+        //assert.equal(err.message,'');
+        done();
+    });
+});
